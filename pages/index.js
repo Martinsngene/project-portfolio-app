135e5a15487d520f41b12a10fab6e1e489933317
Martins Ngene <martinsngene@gmail.com>
2022-01-10 02:22:38 +0100

    [update] on existing project

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          <span className="bg-red-500">Welcome to</span>{" "}
-          <a href="https://nextjs.org">Next.js!</a>
+          <span>Welcome to</span> <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
